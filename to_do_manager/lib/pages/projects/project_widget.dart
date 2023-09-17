437dad1f113bd4e83722eb6884321e2919d4fd3d
Sumukha U <sumukhabhat02@gmail.com>
2023-09-17 08:24:20 +0530

    Projects added

---
+++ to_do_manager/lib/pages/projects/project_widget.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class ProjectPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ProjectBloc projectBloc = BlocProvider.of<ProjectBloc>(context);
+    return StreamBuilder<List<Project>>(
+      stream: projectBloc.projects,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ProjectExpansionTileWidget(snapshot.data!);
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+}
+
+class ProjectExpansionTileWidget extends StatelessWidget {
+  final List<Project> _projects;
+
+  ProjectExpansionTileWidget(this._projects);
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      key: ValueKey(SideDrawerKeys.DRAWER_PROJECTS),
+      leading: Icon(Icons.book),
+      title: Text("Projects",
+          style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold)),
+      children: buildProjects(context),
+    );
+  }
+
+  List<Widget> buildProjects(BuildContext context) {
+    List<Widget> projectWidgetList = [];
+    _projects.forEach((project) => projectWidgetList.add(ProjectRow(project)));
+    projectWidgetList.add(ListTile(
+      key: ValueKey(SideDrawerKeys.ADD_PROJECT),
+      leading: Icon(Icons.add),
+      title: Text("Add Project"),
+      onTap: () async {
+        await context.adaptiveNavigate(SCREEN.ADD_PROJECT, AddProjectPage());
+        context.bloc<ProjectBloc>().refresh();
+      },
+    ));
+    return projectWidgetList;
+  }
+}
+
+class ProjectRow extends StatelessWidget {
+  final Project project;
+
+  ProjectRow(this.project);
+
+  @override
+  Widget build(BuildContext context) {
+    HomeBloc homeBloc = BlocProvider.of(context);
+    return ListTile(
+      key: ValueKey("tile_${project.name}_${project.id}"),
+      onTap: () {
+        homeBloc.applyFilter(project.name, Filter.byProject(project.id!));
+        context.safePop();
+      },
+      leading: Container(
+        key: ValueKey("space_${project.name}_${project.id}"),
+        width: 24.0,
+        height: 24.0,
+      ),
+      title: Text(
+        project.name,
+        key: ValueKey("${project.name}_${project.id}"),
+      ),
+      trailing: Container(
+        height: 10.0,
+        width: 10.0,
+        child: CircleAvatar(
+          key: ValueKey("dot_${project.name}_${project.id}"),
+          backgroundColor: Color(project.colorValue),
+        ),
+      ),
+    );
+  }
+}
+
+class AddProjectPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      bloc: ProjectBloc(ProjectDB.get()),
+      child: AddProject(),
+    );
+  }
+}
